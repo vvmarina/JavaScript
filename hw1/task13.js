4d952db51312cffbd6948846b1c7a6f04045663a
Vizir Marina <vizirmarina@gmail.com>
2022-01-17 23:04:50 +0200

    initial commit

--- hw1/task13.js
+++ hw1/task13.js
@@ -1,12 +1,13 @@
 var arr = [1, 2, 5, 9, 4, 13, 4, 10];
 var message;
 
+outer:
 for (var i = 0; i < arr.length; i++)
 {
   if (arr[i] == 4)
   {
     message = 'Есть!' ;  
-    break;
+    break outer;
    }
  } 
  console.log(message);
