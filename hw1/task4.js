2fe508f3fa5334141e6ebc0eed07d5647a8f9087
Vizir Marina <vizirmarina@gmail.com>
2022-01-17 21:33:48 +0200

    initial commit

---
+++ hw1/task4.js
@@ -0,0 +1,16 @@
+var a = 0;
+
+switch (a) {
+    case 0:
+    console.log( 0 );
+    break;
+    
+    case 1:
+    console.log( 1);
+    break;
+
+    case 2:
+    case 3:       
+    console.log('2,3');
+    break;
+}
