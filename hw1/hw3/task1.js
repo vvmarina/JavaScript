04eaab0611175d502871b7cf367e0ec48b98d4c2
Vizir Marina <vizirmarina@gmail.com>
2022-01-27 21:52:43 +0200

    initial commit

---
+++ hw1/hw3/task1.js
@@ -0,0 +1,20 @@
+var forEach = function(arr, callback) {
+    if (Array.isArray(arr) && typeof callback === 'function') {
+       var i, length = arr.length;
+       for (i = 0; i < length; i = i + 1) {
+        callback(arr[i], i, arr);
+       }
+     }
+          
+        else
+       {
+         throw new Error('The type parametr is not correct')
+
+     }
+    } ;
+    const arr = [1,2,3];
+    forEach(arr , function(item, i, arr) {
+        console.log('Number: ' + item + ', index: ' + i + ',', arr)
+      });
+
+
