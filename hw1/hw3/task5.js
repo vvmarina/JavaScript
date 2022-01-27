5fbac8a42af054cd5edf403c1bb2f9589822dc4d
Vizir Marina <vizirmarina@gmail.com>
2022-01-27 23:53:36 +0200

    commit

---
+++ hw1/hw3/task5.js
@@ -0,0 +1,18 @@
+var reduce = function(arr, callback, startValue) {
+    if (Array.isArray(arr) && typeof callback === 'function'&& (Number.isInteger(startValue)|| typeof startValue === 'string')) {
+    var i, length = arr.length, result = startValue;
+    for (i = 0; i < length; i = i + 1) {
+        result= callback(result, arr[i], i, arr);
+     }
+     return result;
+    }
+   else
+   {
+   throw new Error('The type parametr is not correct')
+   }
+  };
+
+const arr = [1,2,3];
+const acc = 0;
+var sum = reduce(arr, function(acc, item, i, arr) {return item + acc;}, acc);
+console.log(sum);
